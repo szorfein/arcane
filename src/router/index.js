a2be0653fa11941072fe4d80d6611cf1776575f0
szorfein <szorfein@protonmail.com>
2023-11-02 22:53:36 +0000

    start PostList

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PostsView from '../views/PostsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/posts',
+      name: 'posts',
+      component: PostsView
     },
     {
       path: '/about',
