ebfbec45937f2702ef8b93bf369ab1ffec06f607
szorfein <szorfein@protonmail.com>
2023-11-03 15:33:57 +0000

    create test zola/posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,9 @@
-<script setup>
-</script>
-
 <template>
-    <h1>Displaying posts</h1>
+    <div v-for="post in posts" :key="post.id">
+        {{ post.id }} - {{ post.title }}
+    </div>
 </template>
+
+<script setup>
+ import { posts } from '@/zola/posts.js'
+</script>
---
+++ src/zola/posts.js
@@ -0,0 +1,12 @@
+import { reactive } from 'vue';
+
+export const posts = reactive([
+  {
+    id: 1,
+    title: "Use Quickemu",
+  },
+  {
+    id: 2,
+    title: "GURU repo for Gentoo"
+  }
+])
