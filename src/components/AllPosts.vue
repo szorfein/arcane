ff1c0f676af1d81fe53d1771b830291249d48402
szorfein <szorfein@protonmail.com>
2023-11-09 15:46:07 +0000

    last posts on Home view

---
+++ src/components/AllPosts.vue
@@ -0,0 +1,14 @@
+<template>
+    <div v-for="categorie in posts">
+        <h2 class="text-h2">{{ categorie.title }}</h2>
+
+        <div v-for="post in categorie.posts">
+            <h5 class="text-h5">{{ post.title }}</h5>
+            <RouterLink :to="`/post/${ post.slug }`">{{ post.title }}</RouterLink>
+        </div>
+    </div>
+</template>
+
+<script setup>
+ import { posts } from '@/zola-stores/all-posts.js'
+</script>
---
+++ src/zola-stores/all-posts.js
@@ -0,0 +1,13 @@
+import { reactive } from 'vue';
+export const posts = reactive([
+
+{ title: "linux" },
+{ posts: [
+
+{
+  slug: "quickemu",
+  title: "Quickemu",
+},
+
+]},
+])
