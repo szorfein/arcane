e2a35c2c3806692287c163b218f9f8f24a91fc8e
szorfein <szorfein@protonmail.com>
2023-11-09 18:02:28 +0000

    new component: post card

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,10 +1,17 @@
 <template>
-    <div v-for="post in posts" :key="post.id">
-        {{ post.id }} - {{ post.title }}
-        <RouterLink :to="`/post/${ post.slug }`">{{ post.title }}</RouterLink>
-    </div>
+    <v-row>
+        <v-col cols="6" v-for="post in posts" :key="post.id">
+            <PostCard
+                :title="post.title"
+                :url="`/post/${ post.slug }`"
+                :date="post.date"
+                :description="post.description"
+            />
+        </v-col>
+    </v-row>
 </template>
 
 <script setup>
  import { posts } from '@/zola/posts_linux.js'
+ import PostCard from './PostCard.vue'
 </script>
--- src/zola/posts_linux.js
+++ src/zola/posts_linux.js
@@ -5,6 +5,8 @@ export const posts = reactive([
 {
   slug: "quickemu",
   title: "Quickemu",
+  date: "2023-11-05",
+  description: "Replace Oracle Virtualbox by QuickEmu",
   content: `<p>Use Quickemu in order to replace Virtualbox.</p><br><pre data-lang="sh" class="language-sh "><code class="language-sh" data-lang="sh">pacman -S quickemu<br></code></pre><br><p>To downloads the iso for voidlinux:</p><br><pre data-lang="sh" class="language-sh "><code class="language-sh" data-lang="sh">quickemu &lt;arg&gt;<br></code></pre><br>`,
 },
 
