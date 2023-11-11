b8166588b7b97b74f93d2d197e28b24b4a7e911e
szorfein <szorfein@protonmail.com>
2023-11-11 17:23:37 +0000

    release ok

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <h2 class="text-h2">This is an about page</h2>
+  </v-container>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+<style scoped>
 </style>
