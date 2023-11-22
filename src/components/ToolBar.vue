a098886907673665bcf15a5de9716a8c216bfaa1
szorfein <szorfein@protonmail.com>
2023-11-22 12:08:58 +0000

    add tag in toolbar

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,15 +1,26 @@
 <template>
-    <v-toolbar
-        class="bg-surface"
-    >
+    <v-toolbar>
         <v-btn icon @click="$router.back()">
             <v-icon>mdi-arrow-left</v-icon>
         </v-btn>
 
+        <v-toolbar-title>
+            <v-btn :to="`/tag/${ tag }`" variant="text">
+                {{ tag }}
+            </v-btn>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
     </v-toolbar>
 </template>
 
 <script setup>
  import { useRouter } from 'vue-router'
- const router = useRouter();
+ const props = defineProps({
+     tag: String,
+ })
 </script>
