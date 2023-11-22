4c8a7d90e0ca362863713592560fa719d127db40
szorfein <szorfein@protonmail.com>
2023-11-22 17:06:28 +0000

    basic search bar

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,26 @@
 <template>
+    <v-toolbar color="surface">
+        <v-responsive max-width="344">
+            <v-text-field
+                hide-details
+                clearable
+                density="compact"
+                label="Search for..."
+                prepend-icon="mdi-magnify"
+                v-model="searchTerm"
+                single-line
+                type="text"
+            ></v-text-field>
+        </v-responsive>
+    </v-toolbar>
     <v-row>
         <v-col
             cols="12"
             xs="12"
             sm="6"
             md="4"
-            v-for="post in posts"
-            :key="post.date"
+            v-for="(post, index) in getFilteredBlogs()"
+            :key="index"
         >
             <PostCard
                 :title="post.title"
@@ -20,6 +34,14 @@
 </template>
 
 <script setup>
+ import { ref } from 'vue'
  import { posts } from '@/zola-stores/posts_linux.js'
+ const searchTerm = ref('')
  import PostCard from './card/post.vue'
+
+ const getFilteredBlogs = () => {
+     return posts.filter(post => post.title.toLowerCase()
+                                     .includes(searchTerm.value.toLowerCase())
+     );
+ };
 </script>
